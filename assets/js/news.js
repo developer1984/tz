0a687ec167929001accdc850eda80531f1197c6e
Stanisław Romek <info@happy.ovh>
2018-07-29 17:37:00 +0200

    Przebudowa projektu i całe RWD. Sprawdzone na IE, FF, Chrome, Opera

--- assets/js/news.js
+++ assets/js/news.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
     
 //  pokazywanie i ukrywanie wstępu posta oraz ikony    
     $(".news-box-content").hover(function(){
-        $(this).find('.news-description').show(300);        
+        $(this).find('.news-description').show(300);          
     }, function(){
         $(this).find('.news-description').hide(300);
-    });        
+    });            
     $(".news-box-content").hover(function(){        
-        $(this).find('.icons').show(300);
+        $(this).find('.news-icons .icons').show(300);
     }, function(){        
-        $(this).find('.icons').hide(300);
+        $(this).find('.news-icons .icons').hide(300);
     });     
     
     $(".news-box-content1").hover(function(){
