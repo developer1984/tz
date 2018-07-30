6e4d10b5369a7e06d94e0f10459581297d663bec
Jacek-dev <jbulzak@op.pl>
2018-07-30 20:37:15 +0200

    aa

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,16 @@ $(document).ready(function () {
 		};
 	};
 	mobileMenuToggle();
-	$(window).resize(mobileMenuToggle)
+	var setDimensions = function () {
+		var vw = $(window).width();
+		var x = Math.min(4, Math.floor(0.5 + 0.005*vw));
+		$('.slicknav_icon-bar').css('height', x + 'px');
+		var y = Math.min(5, Math.floor(3 + 0.0033*vw));
+		$('.slicknav_icon-bar + .slicknav_icon-bar').css('margin-top', y + 'px');
+	}
+	setDimensions();
+	$(window).resize(function () {
+		mobileMenuToggle();
+		setDimensions();
 	});
+});
