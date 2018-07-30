f8bc6755469f0ee114368dc8b2ac5a06b1fc0cc9
Stanisław Romek <info@happy.ovh>
2018-07-30 18:12:12 +0200

    Aktualizacja boxów i wyświetlania na różnych przeglądarkach

--- assets/js/news.js
+++ assets/js/news.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 //  pokazywanie i ukrywanie wstępu posta oraz ikony    
     $(".news-box-content").hover(function () {
         $(this).find('.news-description').show(300);
-        $(this).find('.news-icons .icons').show(300);
+        $(this).find('.news-icons .icons').show(300);        
     }, function () {
         $(this).find('.news-description').hide(300);
         $(this).find('.news-icons .icons').hide(300);
@@ -17,6 +17,8 @@ $(document).ready(function () {
         $(this).find('.news-description').hide(300);
         $(this).find('.news-icons1 .icons').hide(300);
     });
-   
+
+
+
 
 })
