6ba1b5215feb033a36a514969a607903076eaae0
LukReaver <l88uka@gmail.com>
2018-08-06 21:57:09 +0200

    search html&css&js mod

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,61 +1,55 @@
 $(document).ready(function() {
   "use strict";
 
-   var number = 7;
+  var listLenght = 5;
+  var delayPost = 300;
+  var minChar = 3;
 
-   $('#searchInput').keyup(function(e) {
-      var search = $(this).val().toLowerCase();
-      var searchlength = $(this).val().length;
-        if (searchlength >= 3) {
-              var data = '?search='+search;
-              $.post('https://coderstrust.codingplatform.pl/application/request.php',{search: search},function(e) {
-                          $('.res').html('');
-                        var response = $.parseJSON(e);
-                        var o = 0;
-                        $.each(response["response"]["channels"],function(key,value) {
-                          if (o === number) {
-                               return false;
-                          }
-                            var title = value.title;
-                            var link = value.link;
-                            $('.res').append('<p><a href="'+link+'">'+title+'</a></p>');
+  function clsInput_and_list() {
+    $("#searchInput").val("");   
+    $("#res").empty();
+  }
 
+  $("#searchInput").on("keyup click", function(event) {
+  // $("#searchInput").bind("keyup", " click", function(event) {
+    event.preventDefault();
+    var search = $(this)
+      .val()
+      .toLowerCase();
+    var searchlength = $(this).val().length;
 
-                              o++;
-                        })
+    setTimeout(function() {
+      if (searchlength >= minChar) {
+        // var data = "?search=" + search;
+        $.post(
+          "https://coderstrust.codingplatform.pl/application/request.php",
+          { search: search },
+          function(data) {
+            $("#res").html("");
+            var response = $.parseJSON(data);
+            $.each(response["response"]["channels"], function(key, value) {
+              if (key === listLenght) {
+                return false;
+              } else {
+                var title = value.title;
+                var link = value.link;
+                $("#res").append('<p><a target="_blank" href="'+link +'">'+title +"</a></p>");                
+              }
+            });
+          }
+        );
 
-              })
+      } else if (searchlength < minChar) {      
+        $("#res").empty();
+      }
+    }, delayPost);    
+        
+  });
 
-
-
-        }
-   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // ===opoznienie  blura wysylajac z listy 
+  $("#searchInput").blur(function() {
+    setTimeout(function() {
+      clsInput_and_list();
+    }, 200);
+  });
 });
