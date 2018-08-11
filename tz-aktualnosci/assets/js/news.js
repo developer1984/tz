f977aa09c62bfcba67e2d589a3d19df7bd2d9f29
Stanisław Romek <info@happy.ovh>
2018-08-11 18:58:14 +0200

    poprawka kodu pod IE

--- tz-aktualnosci/assets/js/news.js
+++ tz-aktualnosci/assets/js/news.js
@@ -1,10 +1,11 @@
 $(document).ready(function () { 
 
-    $('#load').click(function() {
-
-        if ($('.line-new:hidden').length > 0) {  
-//             $('.line-new:hidden:eq(0)').show();
-            $('.line-new:hidden:eq(0)').css({display: "flex", height: "100%", transition: "0.5s"})
+    $('#load').click(function() {        
+        if ($('.box-add:hidden').length > 0) {              
+            $('.box-add:hidden:eq(0)').show();
+            $('.box-add:hidden:eq(0)').show();
+            $('.box-add:hidden:eq(0)').show();
+            $('.box-add:hidden:eq(0)').show();
           } else {
               $(this).hide();
         }
